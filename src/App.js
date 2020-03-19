2269602439fc7bcee03d83c106ea91e7398c0c63
LeoFdz <lfernandez79@gmail.com>
2020-03-19 18:03:49 -0500

    init file for table react app, added App components and table component.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1 className="text-primary">Hello, React buddy!</h1>
+            </div>
+        )
+    }
+}
+export default App
