736f5ac2e84500a1349679a2e7898cf86056fc6e
LeoFdz <lfernandez79@gmail.com>
2020-03-19 18:14:31 -0500

    added tablejs component JSX

--- src/table.js
+++ src/table.js
@@ -3,8 +3,38 @@ import React, { Component } from "react"
 class Table extends Component {
 render() {
     return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+        
+            <tbody>
+                <tr>
+                    <td>Lenny</td>
+                    <td>Coder</td>
+                </tr>
+                <tr>
+                    <td>Cindy</td>
+                    <td>Tresury</td>
+                </tr>
+                <tr>
+                    <td>Bella</td>
+                    <td>4th Grader</td>
+                </tr>
+                <tr>
+                    <td>Cece</td>
+                    <td>Kindergarden</td>
+                </tr>
+
+            </tbody>
+        </table>
 
     )
 }
 
 }
+
+export default Table
